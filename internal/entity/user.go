837d66d671e4b660b9d5d0557d8c7596ad4aeeaf
Slamadalius <slamasdalius@gmail.com>
2022-06-23 10:17:28 +0300

    adding user entity

---
+++ internal/entity/user.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"context"
+	"time"
+)
+
+type User struct {
+	ID        string    `json:"_id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Nickname  string    `json:"nickname"`
+	Password  string    `json:"password"`
+	Email     string    `json:"email"`
+	Country   string    `json:"country"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserService interface {
+	CreateUser(ctx context.Context, user User) (err error)
+}
+
+type UserRepository interface {
+	Insert(ctx context.Context, user User) (err error)
+}
